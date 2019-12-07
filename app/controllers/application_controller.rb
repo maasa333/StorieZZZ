437ab85eb814ae1464cb040c15c51fcfe0083e24
maasa333 <konomaasa@gmail.com>
2019-12-07 14:33:20 -0800

    Create new and show actions for dreams

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?, :redirect_if_not_logged_in
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+        if !logged_in?
+            redirect_to "/"
+        end
+    end
 end
