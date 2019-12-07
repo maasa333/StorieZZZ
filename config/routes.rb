160734f4980f4be48b385ea09ad7fe18bd640d4b
maasa333 <konomaasa@gmail.com>
2019-12-07 09:47:20 -0800

    Built helper methods logged_in? and redirect_if_not_logged_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/home'
-  get 'sessions/new'
-  get 'sessions/create'
+  # get 'sessions/home'
+  # get 'sessions/new'
+  # get 'sessions/create'
 
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   root 'sessions#home'
   
+  resources :sessions, only: [:home, :new, :create, :destroy]
   resources :interpretations
   resources :dreams
   resources :users
