e1487c9aaa94f2ce8598287c43f118bb42bf3645
maasa333 <konomaasa@gmail.com>
2019-12-08 14:21:44 -0800

    Change from link to new interpretation from on dream show page

--- app/controllers/interpretations_controller.rb
+++ app/controllers/interpretations_controller.rb
@@ -1,2 +1,24 @@
 class InterpretationsController < ApplicationController
+    def new
+        @interpretation = Interpretation.new
+    end
+    
+    def create
+        @interpretation = current_user.interpretations.build(interpretation_params)
+        if @interpretation.save
+            redirect_to interpretation_path(@interpretation)
+        else
+            render :new    
+        end
+    end
+
+    def show
+        @interpretation = interpretation.find_by(id: params[:id])
+    end
+
+    private
+
+    def interpretation_params
+        params.require(:interpretation).permit(:description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
   
   resources :sessions, only: [:home, :new, :create, :destroy]
   resources :interpretations
-  resources :dreams
-  resources :users
+  resources :dreams do 
+    resources :interpretations, only: [:index, :new, :create]
+  end
+  resources :users do 
+    resources :dreams, only: [:index, :new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
