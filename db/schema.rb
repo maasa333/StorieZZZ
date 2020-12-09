48147ddda91311438a26f139a999b40f2d0d5906
Maasa <maasa.dev@gmail.com>
2020-12-08 21:56:47 -0800

    Update CSS styling and change background image

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_12_08_234940) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "categories", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", precision: 6, null: false
